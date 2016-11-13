f8efa8faca7ba818f15c81e1b6207559aa728eb2
MoonJeounghoon <jeounghoon.moon@gmail.com>
2016-11-14 08:49:44 +0900

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,115 @@
+
+
+
+############################################################################
+## 0. Data Download
+############################################################################
+
+## Download the file and put the file in the data folder
+# if(!file.exists("./data")){dir.create("./data")}
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+
+## Unzip the file
+getwd()
+setwd("c:/data")
+
+#unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+## unzipped files are in the folderUCI HAR Dataset
+path_rf <- file.path("C:/data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+
+
+############################################################################
+## 1. Merges the training and the test sets to create one data set.
+############################################################################
+
+# Read the Activity files
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+str(dataActivityTrain)
+str(dataActivityTest)
+
+# Read Subject files
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+str(dataSubjectTrain)
+str(dataSubjectTest)
+
+# Read Fearures files
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+str(dataFeaturesTrain)
+str(dataFeaturesTest)
+
+
+
+# Merges the training and the test sets to create one data set
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+head(Data)
+
+
+
+##########################################################################################
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+##########################################################################################
+
+# Subset Name of Features by measurements on the mean and standard deviation
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+head(Data)
+str(Data)
+
+
+
+##########################################################################################
+## 3. Uses descriptive activity names to name the activities in the data set
+##########################################################################################
+
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+head(Data$activity, 30)
+
+
+##########################################################################################
+## 4. Appropriately labels the data set with descriptive variable names.
+##########################################################################################
+
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+
+##########################################################################################
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+##########################################################################################
+
+# Creates a second,independent tidy data set and ouput it
+library(plyr)
+Data2<-aggregate(. ~subject + activity, Data, mean)
+
+getwd()
+head(Data2)
+
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
+
+
+
